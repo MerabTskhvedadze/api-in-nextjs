50d3febfcf1dee0568b1b6fa2e7b8e01ddc9a1d6
MerabTskhvedade <merabi.tskhvedadze@gmail.com>
2023-09-18 15:24:42 +0400

    simple api where we simulating relaying information from a third-party secret resource or pretending to get data from a database

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="w-fit mx-auto">Hello, this is api chapters codes</h1>
+    </main>
+  );
+}
